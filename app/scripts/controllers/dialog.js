638675c7c1aa8e375e03b29aea359754faf74ddf
Maarten Lauwaert <maartenlauwaert@gmail.com>
2015-12-02 12:26:37 +0100

    Added template files to dialog
    using ngInclude

---
+++ app/scripts/controllers/dialog.js
@@ -0,0 +1,5 @@
+        $scope.templates = {
+            'style': '../views/templates.dialog.style.html',
+            'filters': '../views/templates.dialog.filters.html',
+            'buttons': '../views/templates.dialog.buttons.html'
+        };
